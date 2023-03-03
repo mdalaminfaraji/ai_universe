76dcdbf60b1626418e065a9625dab9d0a092e4fa
mdalaminfaraji <alaminice1617@gmail.com>
2023-03-03 12:55:00 +0600

    array object validation some complete

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const dataLoad=(dataLimit)=>{
 }
 const displayTools=(tools, dataLimit)=>{
   const toolsContainer=document.getElementById('container');
+  toolsContainer.textContent='';
+  console.log(tools);
+
+
+
 //   show all section start
    const showAll=document.getElementById('show-all');
     if(dataLimit && tools.length>5){
@@ -29,8 +34,8 @@ const displayTools=(tools, dataLimit)=>{
       <div class="card-body">
       <h2>Features</h2>
       <p>1. ${x?x:'No data found'}</p>
-      <p>2. ${y?y:'No data found'}}</p>
-      <p>3. ${z?z:'No data found'}}</p>
+      <p>2. ${y?y:'No data found'}</p>
+      <p>3. ${z?z:'No data found'}</p>
       </div>
       <div class="card-footer">
       <h5 class="card-title">${name}</h5>
@@ -56,6 +61,25 @@ const displayTools=(tools, dataLimit)=>{
   toggleSpinner(false);
 
 }
+// sort by date section
+const fetchDate=()=>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res=>res.json())
+    .then(data=>displayDate(data.data.tools));
+}
+const displayDate=(tools)=>{
+  console.log(tools);
+  const arr1=tools.map(obj=>{
+    return {...obj, published_in:new Date(obj.published_in)};
+  });
+  const sortedAsc=arr1.sort(
+    (objA, objB)=>Number(objA.published_in) - Number(objB.published_in),
+  );
+  console.log(sortedAsc);
+ 
+
+}
+
 // single data details show with model
 const loadToolsDetails=(id)=>{
     // console.log(id)
@@ -68,30 +92,76 @@ const loadToolsDetails=(id)=>{
 const showModalToolsData=(toolsDetail)=>{
     console.log(toolsDetail);
      const {description, pricing, features, integrations, input_output_examples,image_link, accuracy }=toolsDetail;
+    
     const titleDescription=document.getElementById('title');
     titleDescription.innerText=description;
-    document.getElementById('cost-1').innerHTML=`<span class="text-center text-warning">${pricing[0].price?pricing[0].price:'No data found'}</span><br><span class="text-center text-warning">${pricing[0].plan?pricing[0].plan:'No data found'}</span>`;
+    //  document.getElementById('intergration').textContent='';
+
+    if(Array.isArray(pricing)){
+         document.getElementById('pricing').innerText='';
+      document.getElementById('cost-1').innerHTML=`<span class="text-center text-warning">${pricing[0].price?pricing[0].price:'No data found'}</span><br><span class="text-center text-warning">${pricing[0].plan?pricing[0].plan:'No data found'}</span>`;
     document.getElementById('cost-2').innerHTML=`<span class="text-center text-success">${pricing[1].price?pricing[1].price:'No data found'}</span><br><span class="text-center text-success">${pricing[1].plan?pricing[1].plan:'No data found'}</span>`;
     document.getElementById('cost-3').innerHTML=`<span class="text-center text-primary">${pricing[2].price?pricing[2].price:'No data found'}</span><br><span class="text-center text-primary">${pricing[2].plan?pricing[2].plan:'No data found'}}</span>`;
+    }else{
+        // document.getElementById('price').textContent='';
+        document.getElementById('pricing').innerText=`
+         No data found.It may be free
+        `;
+        
+    };
+    
 
     document.getElementById('li').innerHTML=`
     <li>${features['1'].feature_name?features['1'].feature_name:'No data found'}</li>
     <li>${features['2'].feature_name?features['2'].feature_name:'No data found'}</li>
     <li>${features['3'].feature_name?features['3'].feature_name:'No data found'}</li>
     `;
-     document.getElementById('li-integrations').innerHTML=`
+    if(Array.isArray(integrations)){
+       
+        document.getElementById('li-integrations').innerHTML=`
     <li>${integrations[0]?integrations[0]:'No data found'}</li>
     <li>${integrations[1]?integrations[1]:'No data found'}</li>
     <li>${integrations[2]?integrations[2]:'No data found'}</li>
     `;
-    document.getElementById('accuracy').innerHTML=`
-    <span class="bg-danger p-3 w-25 rounded fs-6 text-light">${accuracy.score*100? (accuracy.score*100 ):'no-score found'} % accuracy</span>
+    }
+    else{
+        document.getElementById('li-integrations').innerHTML='';
+    };
+    // else{
+    //     document.getElementById('intergration').innerText=`
+    //     Integration data no found.Its my be null
+    //     `;
+    // };
+     
+  
+    const displayAccuracy= document.getElementById('accuracy');
+    console.log(typeof accuracy.score)
+
+    // displayAccuracy.classList.add('d-none');
+    if(typeof accuracy.score!= 'number'){
+        displayAccuracy.classList.add('d-none');
+    }else{
+        displayAccuracy.classList.remove('d-none');
+         displayAccuracy.innerHTML=`
+    <span class="bg-danger p-3 w-25 rounded fs-6 text-light">${accuracy.score*100} % accuracy</span>
     `
+    }
+  
     document.getElementById('image').innerHTML=`
     <img src="${image_link[0]}" class="card-img-top rounded-lg" alt="...">
     `
-    document.getElementById('input').innerText=`${input_output_examples[0].input? input_output_examples[0].input:'No data found'}`;
-    document.getElementById('output').innerText=`${input_output_examples[0].output? input_output_examples[0].output:'No data found'}`;
+    if(Array.isArray(input_output_examples)){
+        document.getElementById('input').innerText=`${input_output_examples[0].input? input_output_examples[0].input:'No data found'}`;
+    document.getElementById('output').innerText=`${input_output_examples[0].output? input_output_examples[0].output:'No data found'}`; 
+    }else{
+        document.getElementById('input').innerText=`
+        Can you give any example?
+        `;
+        document.getElementById('output').innerText=`
+        No! Not Yet! Take a break!!!
+        `;
+    };
+   
      
 }
 
